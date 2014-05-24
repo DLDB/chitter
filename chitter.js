0c28c7e2a6e2ef859699049d66175d10dbef8160
DLDB <dpbergquist@gmail.com>
2014-05-24 13:19:23 +0100

    added camera and location buttons

--- chitter.js
+++ chitter.js
@@ -1,2 +1,34 @@
-
+$( document ).ready(function () {
+  $('.statHead').hover( function() {
+    $( this ).animate( {
+      'color' : '#0084B4'
+    }, 'slow');
+  } )
+  $('.chitBox').focus(function () {
+    $( this ).css( "height", "93px" );
+    $( this ).removeAttr('placeholder');
+    $('.spoutSomeChit').css( "height", "400px");
+    $('.chitForm').css("height", "380px");
+    $('.chitButtons').css("margin-top", "10px");
+    $(this).css("color","#000000")
+  });
+  $('.chitBox').blur(function () {
+    if($(".chitBox").val().length < 1) {
+      $(this).css( "height", "32px");
+      $(this).attr("placeholder", "Compose new Cheet...");
+      $('.spoutSomeChit').css( "height", "36px");
+      $('.chitButtons').css("margin-top", "20px");
+    }
+    else {
+      $(this).css("color","#8899B3")
+    }
+  });
+  $( '.addPics' ).hover(function() {
+    $(this).css('padding','4px');
+    $(this).css('border','1px solid #00A6CC')
+  }, function() {
+    $(this).css('border','0');
+    $(this).css('padding','5px');
+  })
+});
 
